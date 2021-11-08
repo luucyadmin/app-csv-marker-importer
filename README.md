bbf56bb8641c3bc2ad866deb8b7b4db9a52e9473
Levi Hechenberger Di Valentino <68319554+levvij@users.noreply.github.com>
2021-11-08 16:02:21 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# CSV Marker Importer
+This is an example luucy plugin.
+
+You'll need [Node JS](https://nodejs.org/en/) installed on your system.
+We recommend using [Visual Studio Code](https://code.visualstudio.com) as your code editor, but any normal editor should work too.
+
+After installing node js, open a terminal and type the following commands ot install the luucy command line interface and typescript:
+```
+$ npm install -g luucy-cli typescript
+```
+
+Open the downloaded directory in your editor and use the following commands to start developing locally!
+
+```
+$ luucy serve
+```
---
+++ plugin.ts
@@ -0,0 +1,45 @@
+const uploader = new Upload("*.csv");
+uploader.onUpload.subscribe(file => {
+    file.toString().then(content => {
+        const lines = content.split("\n");
+
+        let latitudeColumnIndex = 0;
+        let longitudeColumnIndex = 1;
+
+        // check if the first line contains headers
+        if (isNaN(+lines[0].split(",")[0])) {
+            // get and drop the header from the lines array
+            const headers = lines.shift().toLowerCase().split(",");
+
+            // try to set lat/long order automatically by parsing the headers
+            latitudeColumnIndex = headers.findIndex(header => header.includes("lat"));
+            longitudeColumnIndex = headers.findIndex(header => header.includes("lon") || header.includes("lng"));
+
+            if (latitudeColumnIndex == -1 || longitudeColumnIndex == -1) {
+                latitudeColumnIndex = 0;
+                longitudeColumnIndex = 1;
+            }
+        }
+
+        const markers: map.Marker[] = [];
+
+        // add all values to the markers array
+        for (let line of lines) {
+            // skip empty lines
+            if (line.trim()) {
+                const fields = line.split(",");
+
+                // create marker
+                markers.push(new map.Marker(
+                    new map.Position(
+                        +fields[latitudeColumnIndex], 
+                        +fields[longitudeColumnIndex]
+                    )
+                ));
+            }
+        }
+
+        // focus the markers
+        map.focus(markers);
+    });
+});
